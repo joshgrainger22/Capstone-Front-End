d51a0a891eb0d354b1a9a805261e9cf768187e80
joshgrainger22 <joshgrainger22@gmail.com>
2022-05-11 08:44:08 -0500

    controllers setup

---
+++ server/controllers/index.js
@@ -0,0 +1,81 @@
+const { Lakes, Comment } = require('../models')
+
+
+const getAllLakes = async (req, res) => {
+    try {
+      const lakes = await Lakes.find()
+      return res.status(200).json({ lakes })
+    } catch (err) {
+      return res.status(500).send(err.message)
+    }
+    }
+const getAllComments = async (req, res) => {
+    try {
+      const comments = await Comment.find()
+      return res.status(200).json({ comments })
+    } catch (err) {
+      return res.status(500).send(err.message)
+    }
+  }
+  const createPost = async (req, res) => {
+    try {
+      const post = await new Post(req.body)
+      console.log(req.body)
+      await post.save()
+      return res.status(201).json({
+        post
+      })
+    } catch (error) {
+      return res.status(500).json({ error: error.message })
+    }
+  }
+  const createComment = async (req, res) => {
+    try {
+      const comment = await new Comment(req.body)
+      console.log(req.body)
+      await comment.save()
+      res.send(comment)
+    } catch (error) {
+      return res.status(500).json({ error: error.message })
+    }
+  }
+  const getAllPost = async (req, res) => {
+    try {
+      const posts = await Post.find()
+      return res.status(200).json({ posts })
+    } catch (err) {
+      return res.status(500).send(err.message)
+    }
+  }
+  const deleteComment = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleted = await Comment.findByIdAndDelete(id);
+      if (deleted) {
+        return res.status(200).send('Data deleted');
+      }
+      throw new Error('Data not found');
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
+  };
+
+  const updateComment = async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Comment.findByIdAndUpdate(id, req.body, { new: true }, (err, data) => {
+        return res.status(200).json(data);
+      });
+    } catch (error) {}
+  };
+
+module.exports = {
+    getAllLakes,
+    getAllPost,
+    createComment,
+    createPost,
+    getAllComments,
+    deleteComment,
+    updateComment
+
+  }
--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,12 @@
 const { model } = require ('mongoose')
 const LakeSchema = require ('./lakes')
-
+const CommentSchema = require ('./comment')
 
 
 const Lakes = model('lakes', LakeSchema)
+const Comment = model('comment', CommentSchema)
 
 module.exports = {
     Lakes,
-
+    Comment
 }
